6bb0873cf4db1b5ebd51cce47f0478d1254b9000
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2022-05-06 20:42:39 +0200

    Unit tests added

--- scripts/app.test.js
+++ scripts/app.test.js
@@ -1,8 +1,57 @@
 import { test, expect } from '@jest/globals';
-import { checkIfEven } from '.scripts/app.js';
+import { stringToArr, trimmed, checkIfEven, randInt, createRandomArr, createPairs, buildPairsList } from './app.js';
 
-describe('Check if array length is an even number', () => {
-  test('true to equal true', () => {
-    expect(checkIfEven(['Radek ', ' Darek K', 'Darek Kr ', 'Grzesiek ', ' Piotr', ' Janusz', 'Dawid', 'Filip', 'Kajtek', 'Ela'])).toBe(true);
+// import jsdom from 'jsdom';
+
+describe('my test', () => {
+  test('convert string to array', () => {
+    const arr = stringToArr('ola, ala');
+    console.log(arr)
+    expect(arr).toStrictEqual(['ola', ' ala']);
+    expect(arr).not.toStrictEqual(['ola', 'ala']);
   })
+
+  test('trim trailing spaces', () => {
+    const arr = trimmed([' ola' ,' ala']);
+    console.log(arr)
+    expect(arr).toStrictEqual(['ola', 'ala']);
+    expect(arr).not.toStrictEqual(['ola ', 'ala']);
+  })
+
+  test('check for even array length', () => {
+    const result = checkIfEven([' ola' ,' ala']);
+    const result2 = checkIfEven(['ola', 'ala', 'marta']);
+    console.log(result)
+    expect(result).toBe(true);
+    expect(result2).toBe(false);
+  })
+
+  test('generate random integer from array', () => {
+    const result = randInt([' ola' ,' ala', 'ela', 'janek']);
+    const condition = result >= 0 && result <= 3
+    console.log(condition);
+    expect(condition).toBe(true);
+  })
+
+  test('generate randomized array and check the elements', () => {
+    const arrNames = [' ola' ,' ala', 'ela', 'ula'];
+    const arr = createRandomArr(arrNames);
+    console.log(arr);
+    expect(arr).toHaveLength(arrNames.length);
+    expect(arr).toContain(' ola');
+    arrNames.forEach(el => {
+      expect(arr).toContain(el);
+    })
+  })
+
+  test('generate array of arrays containing pairs', () => {
+    const arr = createPairs([' ola' ,' ala', 'ela', 'ula']);
+    console.log(arr);
+    expect(arr).toStrictEqual([[' ola', ' ala'],['ela', 'ula']]);
+  })
+
+  // test('', () => {
+  //   buildPairsList([['ola', 'ala'],['ela', 'ula']], )
+  // })
+
 })
